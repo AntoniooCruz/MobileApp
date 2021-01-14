918f39fd9a4c23653bb60ec65f8414740b3dd603
DESKTOP-OVQRJRS\Ruben <oubinamosteiroruben@outlook.es>
2021-01-14 12:04:27 +0100

    Add MainCompany option (Router)

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import "./css/sidemenu.css"
 import Login from "./components/Login.js"
 import SignIn from "./components/SignIn.js"
 import Main from "./components/Main.js"
+import MainCompany from "./components/MainCompany.js"
 import SignInCompany from "./components/SignInCompany.js"
 
 export default class App extends Component 
@@ -25,6 +26,7 @@ export default class App extends Component
                     <Route exact path="/SignIn" component={SignIn} />
                     <Route exact path="/SignInCompany" component={SignInCompany} />
                     <Route exact path="/Main" component={Main} />
+                    <Route exact path="/MainCompany" component={MainCompany} />
                     <Route path="*" component={Login}/>                            
                 </Switch>
             </BrowserRouter>
