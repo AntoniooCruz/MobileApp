bd634b79bbaf20985fbbf7d4349b5704965031a4
Antonio <acruz9823@gmail.com>
2021-01-22 18:00:19 +0100

    Added user is_admin

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,12 @@
+const { boolean } = require('joi');
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
     username: {type: String, unique: true, required: true},
     password: {type: String, required: true},
     name: {type: String, required: true},
-    phone_number: String
+    phone_number: String,
+    is_admin: {type: boolean, required: true}
 })
 
 module.exports = mongoose.model('User', UserSchema);
