e1f1147db9148382de5df36db9e117e943f6d4c0
Antonio <acruz9823@gmail.com>
2021-01-26 17:46:08 +0100

    Changed Product schema

--- server/schemas/products.js
+++ server/schemas/products.js
@@ -3,9 +3,10 @@ const Joi = require('joi');
 
 const productSchema = Joi.object().keys({
     company_id: Joi.string().required(),
-    price: Joi.string().required(),
+    price: Joi.number().required(),
     name: Joi.string().required().max(20),
     is_available: Joi.boolean().required(),
+    img: Joi.any(),
   });
 
 
